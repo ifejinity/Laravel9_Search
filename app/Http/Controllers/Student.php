788f95ed76c497b367642ff800f28ea64ea453f8
ifejinity <mrlonzanida08@gmail.com>
2023-07-06 22:07:09 +0800

    1st commit

---
+++ app/Http/Controllers/Student.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student as ModelsStudent;
+use Illuminate\Http\Request;
+
+class Student extends Controller
+{
+    public function index(){
+        $students = ModelsStudent::ORDERBY('id', 'asc')->paginate(15);
+        return view('student.index')
+            -> with(compact('students'));
+    }
+
+    public function search(Request $request){
+        $input = $request->input('input');
+        $students = ModelsStudent::WHERE('id', $input)->paginate(15);
+        return view('student.index')
+            -> with(compact('students'));
+    }
+}
---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Jeffrey Lonzanida</title>
+    <link rel="stylesheet" href="/resources/css/app.css">
+</head>
+<body>
+    <form action="{{ route('student.search') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="searchHolder">
+            <input type="number" name="input" placeholder="Search ID">
+            <button type="submit">Search</button>
+        </div>
+    </form>
+
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+        </tr>
+        @foreach ($students as $student)
+            <tr>
+                <td>{{$student->id}}</td>
+                <td>{{$student->first_name}}</td>
+                <td>{{$student->last_name}}</td>
+            </tr>
+        @endforeach
+    </table>
+    {{ $students->links() }}
+</body>
+</html>
